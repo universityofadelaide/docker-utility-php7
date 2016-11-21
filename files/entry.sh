e2f48b40c2c6e2692232c1ca47eeff61593bfc0a
Michael Priest <pingers@gmail.com>
2016-11-21 22:33:53 +1030

    Allow commands to be run at entry time.

--- files/entry.sh
+++ files/entry.sh
@@ -5,7 +5,7 @@ if [ "${USER}" != "root" ]; then
     adduser --disabled-password --uid ${UID} --gecos ${USER} --home /code ${USER}
     echo "${USER} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${USER}
     echo "Defaults env_keep += \"SSH_AUTH_SOCK\"" >> /etc/sudoers.d/${USER}
-    sudo -E -i -u ${USER}
+    sudo -E -i -u ${USER} -- sh -c "${*}"
 else
-    sudo -E -i
+    sudo -E -i -- sh -c "${*}"
 fi
